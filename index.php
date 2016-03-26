e5dae07676f61e145f53c3e55b57fabf89259f2c
elchupacabras <josuemrtj@gmail.com>
2016-03-27 00:24:39 +0100

    retocando un poquito el menu en index.php

--- index.php
+++ index.php
@@ -34,7 +34,48 @@
 </head>
 
 <body>
-    <?php include "sections/header.php"; ?>
+<header id="header" class="glue">
+    <div class="row clearfix">
+        <div class="little-head">
+
+            <div class="social social-head">
+                <a href="http://twitter.com/@loscriptozoos" class="bottomtip" title="Síguenos en Twitter" target="_blank"><i class="icon-twitter"></i></a>
+                <a href="http://facebook.com/loscriptozoos" class="bottomtip" title="Me gusta en Facebook" target="_blank"><i class="icon-facebook"></i></a>
+            </div><!-- end social -->
+
+        </div><!-- little head -->
+    </div><!-- row -->
+
+    <div class="headdown">
+        <div class="row clearfix">
+            <div class="logo bottomtip" title="Los Criptozoos Punk Rock Band">
+                <a href="#"><img src="./crypto_imagenes/cripcrip.png" alt="Los Criptozoos Punk Rock Band"></a>
+            </div><!-- end logo -->
+
+            <nav>
+                <ul class="sf-menu">
+                    <li class="current selectedLava">
+                        <a href="index.php">Inicio<span class="sub">Comienza aquí</span></a>
+                    </li>
+                    <li><a href="cripto-about.php">Sobre la banda<span class="sub">Los Criptozoos</span></a></li>
+                    <li>
+                        <a href="cripto-musica.php">Material<span class="sub">Contenido digital</span></a>
+                        <ul>
+                            <li><a href="cripto-fotos.php">Galeria de fotos</a></li>
+                            <li><a href="cripto-musica">Música</a></li>
+                            <li><a href="cripto-videos.php">Videos</a></li>
+                        </ul>
+
+                    </li>
+                    <li><a href="cripto-eventos.php">Eventos<span class="sub">Próximos eventos</span></a></li>
+                    <li><a href="cripto-contacto.php">Contacto<span class="sub">Contacta con nosotros</span></a>
+                    </li>
+                </ul><!-- end menu -->
+            </nav><!-- end nav -->
+        </div><!-- row -->
+    </div><!-- headdown -->
+</header><!-- end header -->
+
 
     <!-- Start Revolution Slider -->
     <div class="sliderr">
